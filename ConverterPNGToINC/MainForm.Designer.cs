78104ccd78a78d60e39894a501c471028911d594
Pavel Halukha <pavelgaluha@gmail.com>
2024-12-15 18:49:25 +0300

    add

---
+++ ConverterPNGToINC/MainForm.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace ConverterPNGToINC
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OpenPngButton = new System.Windows.Forms.Button();
+            this.OpenFolderButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // OpenPngButton
+            // 
+            this.OpenPngButton.Location = new System.Drawing.Point(12, 12);
+            this.OpenPngButton.Name = "OpenPngButton";
+            this.OpenPngButton.Size = new System.Drawing.Size(282, 61);
+            this.OpenPngButton.TabIndex = 0;
+            this.OpenPngButton.Text = "Открыть png файл";
+            this.OpenPngButton.UseVisualStyleBackColor = true;
+            this.OpenPngButton.Click += new System.EventHandler(this.OpenPngButton_Click);
+            // 
+            // OpenFolderButton
+            // 
+            this.OpenFolderButton.Location = new System.Drawing.Point(12, 86);
+            this.OpenFolderButton.Name = "OpenFolderButton";
+            this.OpenFolderButton.Size = new System.Drawing.Size(282, 61);
+            this.OpenFolderButton.TabIndex = 1;
+            this.OpenFolderButton.Text = "Открыть папку с png файлами";
+            this.OpenFolderButton.UseVisualStyleBackColor = true;
+            this.OpenFolderButton.Click += new System.EventHandler(this.OpenFolderButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 159);
+            this.Controls.Add(this.OpenFolderButton);
+            this.Controls.Add(this.OpenPngButton);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ConverterPNGToINC";
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+
+        private System.Windows.Forms.Button OpenFolderButton;
+
+        private System.Windows.Forms.Button OpenPngButton;
+
+        #endregion
+    }
+}
---
+++ ConverterPNGToINC/MainForm.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConverterPNGToINC
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        
+        private void OpenPngButton_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+            {
+                return;
+            }
+
+            CreateInc(openFileDialog1.FileName);
+        }
+
+        private void OpenFolderButton_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.Cancel)
+            {
+                return;
+            }
+            
+            string[] files = Directory.GetFiles(folderBrowserDialog1.SelectedPath);
+            foreach (string file in files)
+            {
+                if (Path.GetExtension(file) == ".png")
+                {
+                    CreateInc(file);
+                }
+            }
+        }
+        
+        private void CreateInc(string pngPath)
+        {
+            string pngName = Path.GetFileNameWithoutExtension(pngPath);
+            string incPath = Path.GetDirectoryName(pngPath) + "\\" + pngName + ".inc";
+            StreamWriter streamWriter = new StreamWriter(incPath);
+            Bitmap bitmap = new Bitmap(pngPath);
+            
+            if (!"123456789".Contains(pngName[pngName.Length - 1]))
+            {
+                streamWriter.WriteLine($"{Regex.Replace(pngName, "([a-z0-9])([A-Z])", "$1_$2").ToUpper()}_WIDTH  = {bitmap.Width}");
+                streamWriter.WriteLine($"{Regex.Replace(pngName, "([a-z0-9])([A-Z])", "$1_$2").ToUpper()}_HEIGHT = {bitmap.Height}");
+            }
+            streamWriter.WriteLine($"{pngName}Texture dd {bitmap.Width}, {bitmap.Height}");
+            streamWriter.WriteLine("db\\");
+            
+            for (int y = 0; y < bitmap.Height; y++)
+            {
+                for (int x = 0; x < bitmap.Width; x++)
+                {
+                    Color color = bitmap.GetPixel(x, y);
+                    if (color.R == 100 && color.G == 100 && color.B == 100)
+                    {
+                        streamWriter.Write("{0, 3},{1, 3},{2, 3}", -1, -1, -1);
+                    }
+                    else if (color.R == 255)
+                    {
+                        streamWriter.Write("{0, 3},{1, 3},{2, 3}", 254, color.G, color.B);
+                    }
+                    else
+                    {
+                        streamWriter.Write("{0, 3},{1, 3},{2, 3}", color.R, color.G, color.B);
+                    }
+
+                    if (x != bitmap.Width - 1 || y != bitmap.Height - 1)
+                    {
+                        streamWriter.Write(",  ");
+                    }
+                }
+                if (y != bitmap.Height - 1)
+                {
+                    streamWriter.WriteLine("\\");
+                }
+            }
+            streamWriter.Close();
+        }
+    }
+}
